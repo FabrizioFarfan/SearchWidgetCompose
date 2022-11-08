2a65b4d12576136da2ee50dc3ed6784a290aa95e
Fabrizio Farfan <farfanfabrizio96@gmail.com>
2022-11-08 23:12:31 +0100

    Search Widget Compose!

---
+++ app/src/main/java/com/fabrizio/searchwidgetcompose/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.fabrizio.searchwidgetcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.fabrizio.searchwidgetcompose.ui.theme.SearchWidgetComposeTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SearchWidgetComposeTheme {
+                MainScreen(mainViewModel = mainViewModel)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/fabrizio/searchwidgetcompose/SearchWidgetState.kt
@@ -0,0 +1,6 @@
+package com.fabrizio.searchwidgetcompose
+
+enum class SearchWidgetState {
+    OPENED,
+    CLOSED
+}
